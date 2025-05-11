611b8ee2c4a950ef9fd932a86bb0be2018941142
hayrialicamdereli <halic89@hotmail.com>
2025-05-11 23:33:12 +0300

    List Personel’s Data

--- FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +12,28 @@ namespace HotelProject.WebUI.Controllers
 {
     public class StaffController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // api consume process
+            // create a client if responsemessage is succeed
+            // set the as a jsondata that coming from sqlserver
+            // deserialize jsondata as a format that monitoring on view 
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5161/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsondata);
+                return View(values);
+            }
+
             return View();
         }
     }
