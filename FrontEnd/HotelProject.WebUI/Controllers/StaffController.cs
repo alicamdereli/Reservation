b11c70a9eebee4485714cb80098d7074ac406525
hayrialicamdereli <halic89@hotmail.com>
2025-05-21 16:58:32 +0300

    update personel information

--- FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/StaffController.cs
@@ -67,6 +67,36 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task <IActionResult> UpdateStaff (int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7009/api/Staff/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateStateViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStateViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(jsonData,Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7009/api/Staff/",content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     } 
 }
 
---
+++ FrontEnd/HotelProject.WebUI/Models/Staff/UpdateStateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HotelProject.WebUI.Models.Staff
+{
+	public class UpdateStateViewModel
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string SocialMedia1 { get; set; }
+        public string SocialMedia2 { get; set; }
+        public string SocialMedia3 { get; set; }
+    }
+}
+
