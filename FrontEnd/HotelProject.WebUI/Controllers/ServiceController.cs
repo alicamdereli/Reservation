29a6044bd45577e61092885b2a2143ba4abe08b4
hayrialicamdereli <halic89@hotmail.com>
2025-08-24 20:21:42 +0300

    servislerin güncellenmesi ve silinmesi

--- FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -62,6 +62,52 @@ namespace HotelProject.WebUI.Controllers
                 return RedirectToAction("Index");
             return View();
         }
+
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient(); 
+            var responseMessage = await client.DeleteAsync($"https://localhost:7009/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7009/api/Service/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateServiceDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7009/api/Service/", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
 
